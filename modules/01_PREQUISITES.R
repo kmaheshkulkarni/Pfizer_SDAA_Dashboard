faeb3d80110d19da0942649070f27bc864408700
kmaheshkulkarni <mpk27213@gmail.com>
2024-10-27 11:21:45 +0000

    Second Commit

--- modules/01_PREQUISITES.R
+++ modules/01_PREQUISITES.R
@@ -11,8 +11,14 @@ PREQUISITES_UI <- function(id) {
     "",
     value = "Tab1",
     fluidRow(
-      column(3, fileInput(ns("files"), "Upload SDAA Excel File", accept = ".xlsx"))
+      column(3, fileInput(ns("files"), "Upload SDAA Excel File", accept = ".xlsx")),
+      
+      column(3, fileInput(ns("files1"), "Upload Clin Pharma Lead Normal Values", accept = ".xlsx")),
+      
+      column(3, fileInput(ns("files2"), "Upload Treatment Codes File", accept = ".xlsx"))
     ),
+
+    
     fluidRow(
       box(
         title = "SDAA DATA",
@@ -25,6 +31,36 @@ PREQUISITES_UI <- function(id) {
           column(12,
                  align = "center",
                  DT::dataTableOutput(ns("dtout")) %>% withSpinner(color = "#0095FF")
+          )
+        )
+      ),
+      
+      box(
+        title = "Clin Pharma Lead Normal Values",
+        closable = FALSE,
+        width = 12,
+        status = "warning",
+        solidHeader = FALSE,
+        collapsible = TRUE,
+        fluidRow(
+          column(12,
+                 align = "center",
+                 DT::dataTableOutput(ns("dtout1")) %>% withSpinner(color = "#0095FF")
+          )
+        )
+      ),
+      
+      box(
+        title = "Treatment Codes File",
+        closable = FALSE,
+        width = 12,
+        status = "warning",
+        solidHeader = FALSE,
+        collapsible = TRUE,
+        fluidRow(
+          column(12,
+                 align = "center",
+                 DT::dataTableOutput(ns("dtout2")) %>% withSpinner(color = "#0095FF")
           )
         )
       )
@@ -82,6 +118,93 @@ PREQUISITES_server <- function(id) {
       print(uploadedData)
       
     })
+    
+    
+    observeEvent(input$files1,{
+      if(is.null(input$files1))
+      {
+        sendSweetAlert(session = session, title = "Error", text = "No Records Present", type = "error")
+      }
+      else
+      {
+        w$show()
+        
+        output$dtout1 <- DT::renderDataTable({
+          if(str_sub(input$files$name, -4) != "xlsx"){
+            shinyalert("Error.....!", "Please Select .xlsx file.", type = "error")
+          }
+          
+          else
+          {
+            data <- readxl::read_excel(input$files1$datapath)
+            uploadedData(data)
+            # FinalData <- readxl::read_xlsx(input$files$datapath)
+            print(head(uploadedData, 10))
+            # print(colnames(data))
+            datatable(uploadedData(), 
+                      options = list(
+                        dom = 't',
+                        scroller = TRUE,
+                        scrollX = TRUE, 
+                        "pageLength" = 100),
+                      rownames = FALSE
+            )
+          }
+          
+        })
+        
+        w$hide()
+      }
+      print("Uploaded Data")
+      print(uploadedData)
+      
+    })
+    
+    
+    
+    observeEvent(input$files2,{
+      if(is.null(input$files2))
+      {
+        sendSweetAlert(session = session, title = "Error", text = "No Records Present", type = "error")
+      }
+      else
+      {
+        w$show()
+        
+        output$dtout2 <- DT::renderDataTable({
+          if(str_sub(input$files$name, -4) != "xlsx"){
+            shinyalert("Error.....!", "Please Select .xlsx file.", type = "error")
+          }
+          
+          else
+          {
+            data <- readxl::read_excel(input$files2$datapath)
+            uploadedData(data)
+            # FinalData <- readxl::read_xlsx(input$files$datapath)
+            print(head(uploadedData, 10))
+            # print(colnames(data))
+            datatable(uploadedData(), 
+                      options = list(
+                        dom = 't',
+                        scroller = TRUE,
+                        scrollX = TRUE, 
+                        "pageLength" = 100),
+                      rownames = FALSE
+            )
+          }
+          
+        })
+        
+        w$hide()
+      }
+      print("Uploaded Data")
+      print(uploadedData)
+      
+    })
+    
+    
+    
+    
     return(uploadedData)
   })
 }
